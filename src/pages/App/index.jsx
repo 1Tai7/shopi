6cbee7b885131c45489a337c198b87cd7e0b8a0e
Ilce <ilsemilena1706@gmail.com>
2024-05-21 17:41:51 -0500

    add link routes

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -9,23 +9,23 @@ import SingIn from "../SignIn";
 
 import "./App.css";
 
-const AppRoutes = () =>{
- let routes = useRoutes([
-   {
-     path: "/",
-     element: <Home />,
-   },
- ]);
- return routes;
-}
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/*", element: <NotFound /> },
+    { path: "/sing-in", element: <SingIn /> },
+  ]);
+  return routes;
+};
 
 const App = () => {
- 
-
   return (
-<BrowserRouter>
-<AppRoutes/>
-</BrowserRouter>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
